e512344c1cc64d0c932dd6f4799662deb126911a
IdaKam <idabk_91@hotmail.com>
2019-10-15 11:26:41 +0200

    lagt til express og body-parse

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const HTTP_CODES = {
+    NOT_FOUND: 404,
+    OK: 200
+};
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
