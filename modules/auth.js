c4c5e9a386352f44bed1a241a30bd723b45cac56
IdaKam <idabk_91@hotmail.com>
2019-11-22 15:33:46 +0100

    moved auth function to separate file

---
+++ modules/auth.js
@@ -0,0 +1,25 @@
+const secretStash = require("./secrets");
+const jwt = require('jsonwebtoken');
+
+let authObj = {};
+
+authObj.userAuth = function (req, res, next) {
+
+    let token = req.headers['authorization'];
+
+    if (token) {
+        try {
+            authObj.auth = jwt.verify(token, secretStash.secret);
+            next();
+        } catch (err) {
+            console.log(err);
+            res.status(403).json({ msg: "Not a valid token" });
+        }
+    }
+    else {
+        res.status(403).json({ msg: "No token" });
+        
+    }
+}
+
+module.exports = authObj;
